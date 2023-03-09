dd56e7445671122ffde3bc42d27c70c22b8d64ca
Vitor Renan <113404754+Vitao7@users.noreply.github.com>
2023-03-09 16:50:06 -0300

    Exclusão dos componentes usando o 'splash' por Bug

--- Onibus-Totvs/src/app/app-routing.module.ts
+++ Onibus-Totvs/src/app/app-routing.module.ts
@@ -7,14 +7,14 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'splash',
+    loadChildren: () => import('./splash/splash.module').then( m => m.SplashPageModule)
+  },
+
 ];
 
 @NgModule({
--- Onibus-Totvs/src/app/app.component.ts
+++ Onibus-Totvs/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {} 
+  constructor(private router : Router, private platform : Platform) {
+    this.iniciarApp()
+
+  }
+
+
+//Quando a aplixação inicar, será levado para a tela splash ( Imagem )
+   iniciarApp(){
+     this.platform.ready().then(( ) => {
+       this.router.navigateByUrl("splash")
+     })
+  }
 }
+
+
